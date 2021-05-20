4f8957efc77815cb544a1f3d40707c24c95e733b
khanmaster <eashkhan@hotmail.com>
2021-05-20 16:02:49 +0100

    functions added with args

---
+++ python_functions.py
@@ -0,0 +1,28 @@
+# Let's create a basic functions to greet
+# Syntax def name():
+
+# first iterations
+# def greetings():
+#     return "Welcome to Cyber Security! " #
+#
+# print(greetings())
+
+# second iteration
+# def greeting_user(argument):
+#     return "welcome on board!"
+#
+# print(greeting_user("daf"))
+
+# Take user name as input() and display is back to them with greeting messages of your choice
+
+# creating a function that takes 2 args as ints
+
+def add(value1, value2):
+    return value1 + value2
+
+print(add(2, 3))
+
+def subtract(value1, value2):
+    return value1 - value2
+
+print(subtract(9, 5))
